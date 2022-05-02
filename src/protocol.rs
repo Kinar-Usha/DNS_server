51b0b27a741e8b776fe0b95ad1db5f95af801498
Kinar-Usha <kinar.cool@gmail.com>
2022-05-03 01:44:22 +0530

    + Type of query (most used)

--- src/protocol.rs
+++ src/protocol.rs
@@ -107,10 +107,40 @@ impl DnsHeader {
 
         Ok(())
     }
-
-
-
 }
 
+#[derive(PartialEq,Eq,Debug,Clone,Hash,Copy)]
+pub enum QueryType {
+    UNKNOWN(u16),
+    A,
+    NS,
+    CNAME,
+    MX,
+    AAAA,
+
+}
+impl  QueryType {
+    pub fn to_num(&self) -> u16{
+        match *self {
+            QueryType::UNKNOWN(x) => x,
+            QueryType::A => 1,
+            QueryType::NS =>2,
+            QueryType::CNAME =>5,
+            QueryType::MX => 15,
+            QueryType::AAAA =>28,
+        }
+    }
+
+    pub fn from_num(num: u16) -> QueryType{
+        match num {
+            1 => QueryType::A,
+            2 => QueryType::NS,
+            5 => QueryType::CNAME,
+            15 => QueryType::MX,
+            28 => QueryType::AAAA,
+            _ => QueryType::UNKNOWN(num),
+        }
+    }
+}
 
 
