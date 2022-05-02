745f575ecb46dbb81846ac3303e5fb7884b56f59
Kinar-Usha <kinar.cool@gmail.com>
2022-05-03 01:55:09 +0530

    + DNS question field

--- src/protocol.rs
+++ src/protocol.rs
@@ -143,4 +143,29 @@ impl  QueryType {
     }
 }
 
+#[derive(Debug,Clone,PartialEq,Eq)]
+pub struct DnsQuestion {
+    pub name: String,
+    pub qtype: QueryType,
+}
 
+impl DnsQuestion{
+    pub fn new(name: String, qtype: QueryType) -> DnsQuestion{
+        DnsQuestion { name: name, qtype: qtype }
+    }
+    pub fn read(&mut self, buffer: &mut BytePacketBuffer) -> Result<()> {
+        buffer.read_qname(&mut self.name)?;
+        self.qtype =QueryType::from_num(buffer.read_u16()?);
+        let _ =buffer.read_u16()?;
+
+        Ok(())
+    }
+
+    pub fn write(&self, buffer: &mut BytePacketBuffer) -> Result<()> {
+        buffer.write_qname(&self.name)?;
+        let type_num = self.qtype.to_num();
+        buffer.write_u16(type_num)?;
+        buffer.write_u16(1)?;
+        Ok(())
+    }
+}
